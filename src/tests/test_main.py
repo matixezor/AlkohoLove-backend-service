701060a8a5d4036ee67057ee839b4ab6c679a35a
Mateusz Romański <mateusz_romanski@interia.pl>
2022-04-03 20:49:16 +0200

    ci: add tests and basic ci [ALKL-16]

---
+++ src/tests/test_main.py
@@ -0,0 +1,11 @@
+import pytest
+from api.main import app
+from httpx import AsyncClient
+
+
+@pytest.mark.anyio
+async def test_root():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"test": "test"}
