d74fdbc2aafe3445b53b8aafadd67fd2c6b39b7a
matixezor <mateusz_romanski@interia.pl>
2022-04-03 20:41:52 +0200

    ALKL-16 | add tests and basic CI

--- src/api/main.py
+++ src/api/main.py
@@ -6,8 +6,8 @@ app = FastAPI(title="FastAPI")
 
 @app.get("/")
 def read_root():
-    return {"123": "ol"}
+    return {"test": "test"}
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:src", host="0.0.0.0", port=8080, reload=True)
