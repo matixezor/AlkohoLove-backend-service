c5f32687665ed47379401f90cfae34b64349e4c0
Thyme1 <73334351+Thyme1@users.noreply.github.com>
2022-04-13 17:13:43 +0200

    feat: create and populate alcohol tables [ALKL-27] (#7)
    * feat: add alcohol tables creation script [ALKL-29]
    
    * style: make table creation more readable [ALKL-29]
    
    * feat: create script to populate alcohol tables [ALKL-30]
    
    * feat: update docker script with database script [ALKL-32]
    
    * feat: add and populate new model of alcohol database [ALKL-27]
    
    * feat: create indexes and constraints on tables [ALKL-29]
    
    * style: change indentations [ALKL-29]

---
+++ init_db/populate_alcohol_tables.sql
@@ -0,0 +1,31 @@
+INSERT INTO alcohol
+VALUES (DEFAULT, 'Żywiec białe', 'beer', 5.0, 'european wheat lager',
+        'Białe piwo pszeniczne z nutą kolendry. Wyróżna się lekkim i orzeźwiającym smakiem. Receptura piwa białego jest dziełem mistrzów żywieckiej szkoły piwowarskiej, którzy od pokoleń pracują w warzelniach arcyksiążęcych w Żywcu i Cieszynie.',
+        1, 4.9, NULL, NULL, 18, 2.4, 11.6, '4-6', 'Żywiec', NULL, NULL),
+       (DEFAULT, 'Soplica Szlachetna Wódka', 'vodka', 4.0, 'czysta',
+        'Jedna z najstarszych wódek w Polsce, to połączenie tradycji i nowoczesności. Wykonana jest z najwyższej klasy spirytusu produkowanego z wielkopolskiego zboża. W trakcie procesu technologicznego poddawana jest czterokrotnej aktywnej filtracji, dzięki czemu ma delikatny smak i zapach. Zastosowanie unikalnych metod oczyszczania spirytusu i zestawiania składników decyduje o jej szczególnym charakterze i harmonijnej kompozycji smakowo-zapachowej. To sprawdzony skład gwarantujący doskonałą jakość.',
+        1,
+        40.0, 'przezroczysty', NULL, NULL, NULL, NULL, NULL, 'Soplica', NULL, NULL);
+
+
+INSERT INTO bar_code
+VALUES ('5900699104827', 1),
+       ('5900471025319', 2);
+
+INSERT INTO aroma
+VALUES (DEFAULT, 'kolendra');
+
+INSERT INTO ingredient
+VALUES (DEFAULT, 'mieszanka zbóż');
+
+INSERT INTO alcohol_aroma
+VALUES (1, 1);
+
+INSERT INTO alcohol_ingredient
+VALUES (1, 1);
+
+INSERT INTO country
+VALUES (DEFAULT, 'Polska');
+
+INSERT INTO region
+VALUES (DEFAULT, 'Polska', 1);
