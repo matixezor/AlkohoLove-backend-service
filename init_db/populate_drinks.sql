c41544a9898a58ea28d696993afc073e2cc83e7d
Thyme1 <konrad1jasinski@gmail.com>
2022-04-21 21:30:59 +0200

    feat: populate vodka and liqueur [ALKL-45]

---
+++ init_db/populate_drinks.sql
@@ -0,0 +1,24 @@
+INSERT INTO alcohol VALUES
+        (
+        --barcodes, region -Uusimaa, Finlandia, flavour ogórek mięta
+        DEFAULT,
+        'Finlandia Botanical',
+        'wódka',
+         NULL,
+        'czysta',
+        'Finlandia Vodka to wódka z najlepszego fińskiego jęczmienia, dojrzewającego w promieniach słońca północy i krystalicznie czystej wody z polodowcowego źródła. W nowoczesnym procesie destylacji powstaje wódka o czystym, delikatnym smaku, idealnie pasująca do koktajli, picia w shotach lub na lodzie.',
+        TODO,
+        30.0,
+        'bezbarwne',
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        NULL,
+        'Finlandia',
+        NULL,
+        TODO
+        ),
