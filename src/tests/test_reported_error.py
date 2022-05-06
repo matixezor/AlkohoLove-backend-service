de66c68955ab15db352e7d6052a935d90d467cee
Thyme1 <konrad1jasinski@gmail.com>
2022-05-06 17:04:33 +0200

    ALKL-59 | fix get tests

--- src/tests/test_reported_error.py
+++ src/tests/test_reported_error.py
@@ -14,25 +14,25 @@ async def test_get_reported_error(
     response = response.json()
     assert response['error_id'] == 1
     assert response['description'] == 'Test error description'
-    print(response)
-    # assert response['user_id'] == 1
+    assert response['user']['username'] == 'admin'
+    assert response['user']['email'] == 'admin@gmail.com'
 
 
-# @mark.asyncio
-# async def test_get_reported_errors(
-#         async_client: AsyncClient,
-#         admin_token: str
-# ):
-#     response = await async_client.get(
-#         '/reported_error?limit=1&offset=0', headers={'Authorization': f'Bearer {admin_token}'}
-#     )
-#     assert response.status_code == 200
-#     response = response.json()
-#     assert len(response['reported_error']) == 1
-#     assert response['page_info']['total'] == 3
-#     assert response['page_info']['limit'] == 1
-#     assert response['page_info']['offset'] == 0
-#     assert response['reported_errors'][0]['error_id'] == 1
-#     assert response['reported_errors'][0]['description'] == 'Test error description'
-#     assert response['reported_errors'][0]['user'][0] == 'admin'
-#     assert response['reported_errors'][0]['user'][1] == 'admin@gmail.com'
+@mark.asyncio
+async def test_get_reported_errors(
+        async_client: AsyncClient,
+        admin_token: str
+):
+    response = await async_client.get(
+        '/reported_error?limit=1&offset=0', headers={'Authorization': f'Bearer {admin_token}'}
+    )
+    assert response.status_code == 200
+    response = response.json()
+    assert len(response['reported_errors']) == 1
+    assert response['page_info']['total'] == 1
+    assert response['page_info']['limit'] == 1
+    assert response['page_info']['offset'] == 0
+    assert response['reported_errors'][0]['error_id'] == 1
+    assert response['reported_errors'][0]['description'] == 'Test error description'
+    assert response['reported_errors'][0]['user']['username'] == 'admin'
+    assert response['reported_errors'][0]['user']['email'] == 'admin@gmail.com'
