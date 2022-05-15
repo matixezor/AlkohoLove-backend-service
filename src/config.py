2e59b28f31532702fcbcd4c2d0b6d5b5ca3ae60d
matixezor <mateusz_romanski@interia.pl>
2022-05-15 13:53:26 +0200

    ALKL-101 | add cors wildcard

--- src/config.py
+++ src/config.py
@@ -7,6 +7,10 @@ DATABASE_URL = getenv('DATABASE_URL')
 SECRET_KEY = '3f23d9abf331784b834b62aa347cfaf7cd1970f0c072d9d4e8595e41f5e708c5'
 ALGORITHM = 'HS256'
 IMAGEDIR = "../alcohol-images/"
+ALLOWED_ORIGINS = ['*']
+ALLOWED_METHODS = ["*"]
+ALLOWED_HEADERS = ["*"]
+ALLOW_CREDENTIALS = False
 
 
 class Settings(BaseModel):
