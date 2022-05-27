eaaa18d542ef5431ec9a55276aad06aac312f67d
Thyme1 <konrad1jasinski@gmail.com>
2022-05-27 22:31:06 +0200

    ALKL-58 | create get favourites by user id

--- src/api/user_list.py
+++ src/api/user_list.py
@@ -5,6 +5,7 @@ from starlette import status
 from src.domain.alcohol import PaginatedAlcohol
 from src.domain.common import PageInfo
 from src.infrastructure.database.database_config import get_db
+from src.infrastructure.database.models.user_list.favourites_database_handler import UserFavouritesHandler
 from src.infrastructure.database.models.user_list.wishlist_database_handler import UserWishlistHandler
 
 router = APIRouter(prefix='/list', tags=['list'])
@@ -30,6 +31,36 @@ async def get_wishlist(
         limit, offset, db.user_wishlist, db.alcohols, user_id
     )
     total = await UserWishlistHandler.count_alcohols_in_wishlist(db.user_wishlist, user_id)
+    return PaginatedAlcohol(
+        alcohols=alcohols,
+        page_info=PageInfo(
+            limit=limit,
+            offset=offset,
+            total=total
+        )
+    )
+
+
+@router.get(
+    path='/favourites/{user_id}',
+    response_model=PaginatedAlcohol,
+    status_code=status.HTTP_200_OK,
+    summary='Read user favourite alcohol list with pagination',
+    response_model_by_alias=False
+)
+async def get_favourites(
+        limit: int = 10,
+        offset: int = 0,
+        user_id: str = None,
+        db: Database = Depends(get_db),
+) -> PaginatedAlcohol:
+    """
+    Show user favourite alcohol list with pagination
+    """
+    alcohols = await UserFavouritesHandler.get_user_favourites_by_user_id(
+        limit, offset, db.user_favourites, db.alcohols, user_id
+    )
+    total = await UserFavouritesHandler.count_alcohols_in_favourites(db.user_favourites, user_id)
     return PaginatedAlcohol(
         alcohols=alcohols,
         page_info=PageInfo(
