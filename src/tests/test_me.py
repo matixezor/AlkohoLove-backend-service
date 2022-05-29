72b5df702ca7086415e41be8ce121ffbc92e8d38
Thyme1 <konrad1jasinski@gmail.com>
2022-05-29 15:34:26 +0200

    ALKL-58 | create tests for reading lists

--- src/tests/test_me.py
+++ src/tests/test_me.py
@@ -1,6 +1,8 @@
 from pytest import mark
 from httpx import AsyncClient
 
+from src.utils.list_fixtures import WISHLIST_FIXTURE, FAVOURITES_FIXTURE, SEARCH_HISTORY_FIXTURE
+
 
 @mark.asyncio
 @mark.parametrize(
@@ -131,3 +133,48 @@ async def test_delete_self(
 ):
     response = await async_client.delete('/me', headers=user_token_headers)
     assert response.status_code == 204
+
+
+@mark.asyncio
+async def test_get_wishlist(
+        async_client: AsyncClient,
+        user_token_headers: dict[str, str]
+):
+    response = await async_client.get('/me/wishlist', headers=user_token_headers)
+    assert response.status_code == 200
+    response = response.json()
+    assert len(response['alcohols']) == 2
+    assert response['page_info']['total'] == 2
+    assert response['page_info']['limit'] == 10
+    assert response['page_info']['offset'] == 0
+    assert response['alcohols'] == WISHLIST_FIXTURE
+
+
+@mark.asyncio
+async def test_get_favourites(
+        async_client: AsyncClient,
+        user_token_headers: dict[str, str]
+):
+    response = await async_client.get('/me/favourites', headers=user_token_headers)
+    assert response.status_code == 200
+    response = response.json()
+    assert len(response['alcohols']) == 2
+    assert response['page_info']['total'] == 2
+    assert response['page_info']['limit'] == 10
+    assert response['page_info']['offset'] == 0
+    assert response['alcohols'] == FAVOURITES_FIXTURE
+
+
+@mark.asyncio
+async def test_get_search_history(
+        async_client: AsyncClient,
+        user_token_headers: dict[str, str]
+):
+    response = await async_client.get('/me/search_history', headers=user_token_headers)
+    assert response.status_code == 200
+    response = response.json()
+    assert len(response['alcohols']) == 2
+    assert response['page_info']['total'] == 2
+    assert response['page_info']['limit'] == 10
+    assert response['page_info']['offset'] == 0
+    assert response['alcohols'] == SEARCH_HISTORY_FIXTURE
