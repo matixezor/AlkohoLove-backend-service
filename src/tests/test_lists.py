ae8d6fcf24f664c1a02f0e3ddfc0a66fd83556f3
Thyme1 <konrad1jasinski@gmail.com>
2022-05-31 20:21:11 +0200

    ALKL-58 | fix totals for lists

--- src/tests/test_lists.py
+++ src/tests/test_lists.py
@@ -42,8 +42,8 @@ async def test_get_search_history(
     response = await async_client.get('/list/search_history/6288e2fdd5ab6070dde8db8c', headers=user_token_headers)
     assert response.status_code == 200
     response = response.json()
-    assert len(response['alcohols']) == 2
+    assert len(response['alcohols_and_dates']) == 2
     assert response['page_info']['total'] == 2
     assert response['page_info']['limit'] == 10
     assert response['page_info']['offset'] == 0
-    assert response['alcohols'] == SEARCH_HISTORY_FIXTURE
+    assert response['alcohols_and_dates'] == SEARCH_HISTORY_FIXTURE
