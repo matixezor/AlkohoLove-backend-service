263fd3711a1b64f2dc0b33f2baf8c8ee95cf8171
Hiiiiiku <adam.skorupa99@gmail.com>
2022-06-02 16:37:10 +0200

    fix registration

--- src/infrastructure/database/models/user_list/search_history.py
+++ src/infrastructure/database/models/user_list/search_history.py
@@ -7,4 +7,4 @@ from src.domain.user_list.search_history_entry import SearchHistoryEntry
 class UserSearchHistory(TypedDict):
     _id: ObjectId
     user_id: ObjectId
-    alcohols_and_dates: list[SearchHistoryEntry]
+    alcohols: list[SearchHistoryEntry]
