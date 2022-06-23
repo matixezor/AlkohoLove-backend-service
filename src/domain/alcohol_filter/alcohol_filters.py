53474e892b32b365b8252b87d81810733380c9c4
matixezor <mateusz_romanski@interia.pl>
2022-06-23 09:09:35 +0200

    ALKL-198 | fix empty kind bug

--- src/domain/alcohol_filter/alcohol_filters.py
+++ src/domain/alcohol_filter/alcohol_filters.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class AlcoholFilters(BaseModel):
@@ -6,3 +6,9 @@ class AlcoholFilters(BaseModel):
     type: list[str] | None
     color: list[str] | None
     country: list[str] | None
+
+    @validator('kind')
+    def validate_kind(cls, value: str) -> str:
+        if not value:
+            raise ValueError('Kind filter must be provided')
+        return value
