b8c45bf5a7f6759713ea2934d47494bf617e4a19
Mateusz Romański <mateusz_romanski@interia.pl>
2022-11-03 22:46:44 +0100

    fix: removing from favourite counter [ALKL-364] (#75)

--- src/infrastructure/database/models/user/user_database_handler.py
+++ src/infrastructure/database/models/user/user_database_handler.py
@@ -189,6 +189,6 @@ class UserDatabaseHandler:
                 'favourites_count': {'$gt': 0}
             },
             {
-                '$set': {'favourites_count': {'$inc': -1}}
+                '$inc': {'favourites_count': -1}
             }
         )
