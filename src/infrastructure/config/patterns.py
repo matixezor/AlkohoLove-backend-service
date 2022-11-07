3e896f8d07530969bacc4704c7b9cf38d2707eb6
matixezor <mateusz_romanski@interia.pl>
2022-11-07 12:49:13 +0100

    fix: email regex [ALKL-364]

--- src/infrastructure/config/patterns.py
+++ src/infrastructure/config/patterns.py
@@ -1,4 +1,4 @@
 from re import compile
 
-email_pattern = compile(r'[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}')
+email_pattern = compile(r'[a-zA-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}')
 password_pattern = compile(r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$')
