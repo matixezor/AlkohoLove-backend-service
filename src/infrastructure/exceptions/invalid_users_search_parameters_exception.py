eee831445aa4defe9b47358cf48f7aac6537f174
matixezor <mateusz_romanski@interia.pl>
2022-12-06 22:03:59 +0100

    feat: context aware user search [ALKL-413]

--- src/infrastructure/exceptions/invalid_users_search_parameters_exception.py
+++ src/infrastructure/exceptions/invalid_users_search_parameters_exception.py
@@ -5,5 +5,5 @@ class InvalidUsersSearchParametersException(HTTPException):
     def __init__(self):
         super().__init__(
             status.HTTP_400_BAD_REQUEST,
-            'Niepoprawne parametry wyszukiwania'
+            'Niepoprawne parametry wyszukiwania.'
         )
