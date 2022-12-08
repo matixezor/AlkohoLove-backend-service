348e9a03ba404e573d90ca5ebba5e95e05e193d2
matixezor <mateusz_romanski@interia.pl>
2022-12-08 23:29:55 +0100

    feat: more user details in search payload [ALKL-416]

--- src/domain/user/user_social.py
+++ src/domain/user/user_social.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 from src.domain.common import MongoBaseModel
 from src.domain.user.user_social_base import UserSocialBase
 
 
 class UserSocial(MongoBaseModel, UserSocialBase):
-    pass
+    created_on: datetime
+    favourites_count: int
