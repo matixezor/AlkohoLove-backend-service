138bac5d2374363e060a6a3bd1f88797885377a7
matixezor <mateusz_romanski@interia.pl>
2022-12-08 23:33:41 +0100

    feat: more user details in search payload [ALKL-416]

--- src/domain/user/paginated_user_info.py
+++ src/domain/user/paginated_user_info.py
@@ -1,8 +1,13 @@
 from src.domain.common import PageInfo
 from src.domain.common.base_model import BaseModel
-from src.domain.user.user_social import UserSocial
+from src.domain.user.user_social import UserSocial, ExtendedUserSocial
 
 
 class PaginatedUserSocial(BaseModel):
     users: list[UserSocial]
     page_info: PageInfo
+
+
+class PaginatedExtendedUserSocial(PaginatedUserSocial):
+    users: list[ExtendedUserSocial]
+
--- src/domain/user/user_social.py
+++ src/domain/user/user_social.py
@@ -5,5 +5,9 @@ from src.domain.user.user_social_base import UserSocialBase
 
 
 class UserSocial(MongoBaseModel, UserSocialBase):
+    pass
+
+
+class ExtendedUserSocial(UserSocial):
     created_on: datetime
     favourites_count: int
