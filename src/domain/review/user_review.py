75ac1d38540f0daff5672eb769e1c82389ef52e8
Thyme1 <konrad1jasinski@gmail.com>
2023-01-15 14:47:30 +0100

    ALKL-510 | add user_id to review response

--- src/domain/review/user_review.py
+++ src/domain/review/user_review.py
@@ -9,6 +9,7 @@ from src.domain.common import MongoBaseModel
 
 class UserReview(MongoBaseModel, ReviewBase):
     username: str
+    user_id: PyObjectId = Field(default_factory=PyObjectId, alias="user_id")
     date: datetime
     alcohol_id: PyObjectId = Field(default_factory=PyObjectId, alias="alcohol_id")
     helpful_count: int
@@ -19,4 +20,8 @@ class UserReview(MongoBaseModel, ReviewBase):
 
     @validator('alcohol_id', always=True)
     def set_alcohol_id(cls, v):
+        return str(v)
+
+    @validator('user_id', always=True)
+    def set_user_id(cls, v):
         return str(v)
